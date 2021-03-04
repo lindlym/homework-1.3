7dd9aaaee69e55ee703633c52786074c9fccfa36
Matt Lindly <matt@arrayschool.com>
2021-03-04 15:42:15 -0700

    Added homework assignment.

---
+++ homework.js
@@ -0,0 +1,62 @@
+/**
+ * Complete the following prompts to finish this homework assignment. Please keep in mind the output must be the exact
+ * same as specified in order to pass. If it says 0, 2, 4 as an output, but your console output is:
+ * 
+ * 0
+ * 2
+ * 4
+ * 
+ * ... then you will be marked points. The output must be exactly as specified! Any questions can be directed to 
+ * a professor. Good luck!
+ */
+
+/**
+ * PROBLEM:
+ * Complete the function below so that when executed, it will print every even number from 0 until the parameter, 
+ * printing both the 0 and the final integer, if it is an even number.
+ * 
+ * EXAMPLE OUTPUT:
+ * printEvenIntegers(12), Output: 0, 2, 4, 6, 8, 10, 12
+ */
+
+ function printEvenIntegers(value) {
+     // Replace this comment with code.
+ }
+
+printEvenIntegers(12); // Output: 0, 2, 4, 6, 8, 10, 12
+printEvenIntegers(1); // Output: 0
+printEvenIntegers(21); // Output: 0, 2, 4, 6, 9, 10, 12, 14, 16, 18, 20
+
+/**
+ * PROBLEM:
+ * Complete the function below so that when executed, it will print the factorial of the number input. The factorial
+ * of a number is when you multiply all whole numbers from our chosen number down to 1.
+ * 
+ * EXAMPLE OUTPUT:
+ * printFactorial(4), Output: 24 (NOTE: 4 * 3 * 2 * 1 = 24)
+ */
+
+ function printFactorial(value) {
+     // Replace this comment with code.
+ }
+
+ printFactorial(1); // Output: 1
+ printFactorial(4); // Output: 24
+ printFactorial(10); // Output: 3628800
+
+ /**
+  * PROBLEM:
+  * Complete the function below so that when executed, determines if the two inputted strings are palindromes or not.
+  * A palindrome is when a word can be read the same forwards and backwards. "Racecar" is a palindrome.
+  * 
+  * EXAMPLE OUTPUT:
+  * isPalindrome('racecar', 'sweet'), Output: racecar is a palindrome, sweet is not a palindrome.
+  */
+
+  function isPalindrome(string1, string2) {
+      // Replace this comment with code.
+  }
+
+  isPalindrome('racecar', 'trouble'); // Output: racecar is a palindrome, trouble is not a palindrome.
+  isPalindrome('honey', 'tacos'); // Output: honey is not a palindrome, tacos is not a palindrome.
+  isPalindrome('madam', 'mom'); // Output: madam is a palindrome, mom is a palindrome.
